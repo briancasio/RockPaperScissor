1e4b9c9cb91b1b25483946c20c7292d13102dfb3
briancasio <113497684+briancasio@users.noreply.github.com>
2023-11-28 14:39:04 -0700

    create MethodDemo.java, .class, filename.txt, transformation, decrypt fiel

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+
+
+const rock = document.querySelector("rock");
+const paper = document.querySelector("paper");
+const scissors = document.querySelector("scissors");
+
+function computerPlay(){
+    let choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random()*choices.length)]
+    if (choices == 0){
+    
+    }
+}
+
+
+
+
+
+
+/*
+function getComputerChoice(){  
+    let computerSelection = Math.floor(Math.random()*3);
+    switch(computerSelection){
+
+        case 0:
+        return "Rock";
+        case 1:
+        return "Paper";
+        case 2:
+        return "Scissors";
+    }
+}
+
 function getPlayerSelection(){ 
     alert("Type Rock, Paper or Scissors")
     const playerSelection = prompt().toLowerCase;
@@ -6,10 +39,6 @@ function getPlayerSelection(){
 
 const playerSelection = getPlayerSelection();
 
-function getComputerChoice(){  
-    const computerSelection = "paper";
-    return computerSelection;
-}
 
 const computerSelection = getComputerChoice();
 
@@ -21,3 +50,4 @@ function playRound(playerSelection, computerSelection){
 playRound(playerSelection, computerSelection);
 
 
+*/
